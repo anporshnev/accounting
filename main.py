83033a350279d716d41f35bb3636286d645451f9
AndreyPorshnev <andrew.porshnev@yandex.ru>
2026-02-02 14:14:08 +0300

    Добавляет структуру

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from accounting!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
