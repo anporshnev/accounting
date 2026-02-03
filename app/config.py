d10c29e71ac97f9668401856e29f1d093369275e
AndreyPorshnev <andrew.porshnev@yandex.ru>
2026-02-03 14:44:06 +0300

    Добавляет и настраивает миграции с alembic

--- app/config.py
+++ app/config.py
@@ -4,26 +4,32 @@ from dotenv import find_dotenv
 
 
 class DBSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=find_dotenv(), extra='ignore')
+    
     db_name: str
     db_user: str
     db_password: SecretStr
     db_host: str
     db_port: int
 
-    model_config = SettingsConfigDict(env_file=find_dotenv(), extra='ignore')
-
     @property
     def db_url(self):
         return (f"postgresql+asyncpg://"
                 f"{self.db_user}:{self.db_password.get_secret_value()}@{self.db_host}:{self.db_port}/{self.db_name}")
+        
+    @property
+    def sync_db_url(self):
+        return (f"postgresql+psycopg2://"
+                f"{self.db_user}:{self.db_password.get_secret_value()}@{self.db_host}:{self.db_port}/{self.db_name}")
 
 class SecuritySettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=find_dotenv(), extra='ignore')
+    
     secret_key: SecretStr = Field(validation_alias='SEC_SECRET_KEY')
     algorithm: str = Field(validation_alias='SEC_ALGORITHM')
     access_token_expire: int = Field(validation_alias='SEC_ACCESS_TOKEN_EXPIRE_MINUTES')
     refresh_token_expire: int = Field(validation_alias='SEC_REFRESH_TOKEN_EXPIRE_MINUTES')
-
-    model_config = SettingsConfigDict(env_file=find_dotenv(), extra='ignore')
+ 
 
 class Settings(BaseSettings):
     db_settings: DBSettings = DBSettings()
