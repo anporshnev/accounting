83c3b52d039d75d0fe207e850ea458fb6916ad56
AndreyPorshnev <andrew.porshnev@yandex.ru>
2026-02-04 16:45:24 +0300

    Переносит поля из миксина в базовый класс модели

--- app/models/room.py
+++ app/models/room.py
@@ -1,12 +1,11 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 
-from app.mixins.models_mixins import IDMixins
 from app.models.base import Base
 
 
 
-class Room(IDMixins, Base):
+class Room(Base):
     __tablename__ = 'rooms'
 
     title: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
