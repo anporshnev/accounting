93428432970d1ecbe48c95ed1b490ba1cd74d4fb
AndreyPorshnev <andrew.porshnev@yandex.ru>
2026-02-06 13:30:48 +0300

    Добавляет связь в модель

--- app/models/group.py
+++ app/models/group.py
@@ -1,8 +1,9 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
 
 from app.mixins.id_mixins import IdMixin
 from app.models.base import Base
+from app.models.device import Device
 
 
 
@@ -10,3 +11,4 @@ class Group(Base, IdMixin):
     __tablename__ = 'groups'
     
     title: Mapped[str] = mapped_column(String(100), unique=True)
+    devices: Mapped[list["Device"]] = relationship(back_populates="group")
