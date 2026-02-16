876a6d0d9a70ec1124a46064189f816a2f8e3c9d
AndreyPorshnev <andrew.porshnev@yandex.ru>
2026-02-16 14:38:00 +0300

    Регистрирует роуты group

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from app.routers.room import room_router
+from app.routers.group import group_router
 
 app = FastAPI()
 
 app.include_router(room_router)
+app.include_router(group_router)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True)
